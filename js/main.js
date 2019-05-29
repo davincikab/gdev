3793eddf61d2a4f4efbc321a9e7963ce9923404b
davincikab <davidnganganjeri079@gmail.com>
2019-05-29 19:56:33 +0300

    Commenting the program

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
+// Declare a map variable: pass the map container id, set the center and the zoom level
 let map = L.map('map').setView([ -0.0723876953125,36.056884765625], 12);
 
+// add a tilelayer: provide the url and the attribution
 L.tileLayer()
+
+// Create a marker: provide a center, popup
 let marker = L.marker([-0.2345,37.668]).bindPopup('Home').addTo(map);
 
+// Working with geometry: Point, polyline and polygon
 let polyline = L.polyline([[-0.2345,37.668],[-0.345,37.668],[-0.45,37.668]]).addTo(map);
-// let polygon = L.polygon([[[-0.5345,37.668],[-0.745,37.68],[-0.45,36.668],[-0.5345,37.668]],[]]).addTo(map);
+let polygon = L.polygon([[-0.5345,37.668],[-0.745,37.68],[-0.45,36.668],[-0.5345,37.668]]).addTo(map);
 
+
+// Working with geojson layer
 let geojson_layer = L.geoJson([nakuru]);
+
+// add the geojson_layer to the map
 map.addLayer(geojson_layer);
